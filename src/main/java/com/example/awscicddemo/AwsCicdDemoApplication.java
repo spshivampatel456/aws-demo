95021650f0ce4b9af23c2fa015cf6ad84c7c05c8
spshivampatel456 <spshivampatel456@gmail.com>
2023-08-19 17:21:18 +0530

    initial commit

---
+++ src/main/java/com/example/awscicddemo/AwsCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awscicddemo/AwsCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
