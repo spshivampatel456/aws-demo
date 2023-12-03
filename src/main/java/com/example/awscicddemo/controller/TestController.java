7a02b7cb71209bbc8633f2d89b38fe376eb2d4ad
spshivampatel456 <55949248+spshivampatel456@users.noreply.github.com>
2023-12-03 12:01:49 +0530

    Update TestController.java

--- src/main/java/com/example/awscicddemo/controller/TestController.java
+++ src/main/java/com/example/awscicddemo/controller/TestController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Hello world Test Cicd deploy";
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello world!!!";
     }
 
 }
